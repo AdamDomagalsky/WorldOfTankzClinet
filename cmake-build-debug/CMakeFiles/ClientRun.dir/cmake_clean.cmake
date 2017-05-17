cb5df8fcd840000049418028298503511877a0c7
AdamDomagalsky <adadom@st.amu.edu.pl>
2017-05-17 14:20:07 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/ClientRun.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClientRun.dir/client.cpp.o"
+  "ClientRun.pdb"
+  "ClientRun"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClientRun.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/WorldOfTankzClinet.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WorldOfTankzClinet.dir/main.cpp.o"
+  "WorldOfTankzClinet.pdb"
+  "WorldOfTankzClinet"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WorldOfTankzClinet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
